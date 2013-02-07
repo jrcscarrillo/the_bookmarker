f7b17ad54ef9bb13c7a7bd98432677aaca597f4c
Juan Carrillo <jcarrillo1@madisoncollege.edu>
2013-02-07 09:31:32 -0600

    The Bookmarker 1st. commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheBookmarker::Application.config.secret_token = '9846a70245f1684d951c24def2d2545d99030e135801cde16780e920f4f9d55787d1cd5082223d749ccce9ca5fef68642de5c85c7e81a0bdbf1dd88c485d8738'
